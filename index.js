443dfa36032025bb2d59ecebbacda80e1f40c7a6
GISELLY LIMA DA SILVA <GISELLY1066@DIGITALCOLLEGE.LOCAL>
2024-09-23 11:41:07 -0300

    adicionados

---
+++ index.js
@@ -0,0 +1,9 @@
+const { default: axios } = require("axios");
+
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao buscar os posts:', error);
+  });
